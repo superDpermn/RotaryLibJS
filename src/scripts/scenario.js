d7a7a28821ed7e9d995f47d4856aa8b386741967
sulo888 <slymnkz364@gmail.com>
2024-06-02 21:59:14 +0300

    scenario 6-7 impleted

--- src/scripts/scenario.js
+++ src/scripts/scenario.js
@@ -16,7 +16,20 @@ export function scenario2() {
   SCENARIO.AddGear(16, SCENARIO.initGear, "DOWN");
   SCENARIO.AddGear(16, SCENARIO.initGear, "UP");
 }
+
 export function scenario3() {
+  let s1 = SCENARIO.AddShaft(SCENARIO.initGear, 1, "OVER");
+  let g1 = SCENARIO.AddGear(16, s1);
+  SCENARIO.AddGear(24, g1, "RIGHT");
+  SCENARIO.AddGear(32, g1, "LEFT");
+  SCENARIO.AddGear(16, g1, "UP");
+  SCENARIO.AddGear(12, SCENARIO.initGear, "DOWN");
+  let s2 = SCENARIO.AddShaft(g1, 2, "OVER");
+  let g2 = SCENARIO.AddGear(20, s2);
+  SCENARIO.AddGear(12, g2, "RIGHT");  
+}
+
+export function scenario4() {
   let s1 = SCENARIO.AddShaft(SCENARIO.initGear, 2, "OVER");
   SCENARIO.AddGear(8, s1);
   SCENARIO.AddGear(12, s1);
@@ -25,21 +38,30 @@ export function scenario3() {
   SCENARIO.AddBelt(
     SCENARIO.gearArr[SCENARIO.gearArr.length - 2],
     SCENARIO.gearArr[SCENARIO.gearArr.length - 1]
-  );
+);  
 }
 
-export function scenario4() {
+export function scenario5() {
   let s1 = SCENARIO.AddShaft(SCENARIO.initGear, 1, "UNDER");
   let g1 = SCENARIO.AddGear(56, s1);
   SCENARIO.AddGear(24, g1, "LEFT");
   SCENARIO.AddGear(16, SCENARIO.initGear, "DOWN");
 }
 
-export function scenario5() {
+export function scenario6() {
+  const g10 = SCENARIO.AddGear(24, SCENARIO.initGear, "LEFT");
+  const g6 = SCENARIO.AddGear(16, g10, "UP");
   let s1 = SCENARIO.AddShaft(SCENARIO.initGear, 1, "OVER");
   let g1 = SCENARIO.AddGear(16, s1);
   let g2 = SCENARIO.AddFreeGear(16, { x: 0, y: 2, z: 12 });
   SCENARIO.AddBelt(g1, g2);
+}
+
+export function scenario7() {
+  let s1 = SCENARIO.AddShaft(SCENARIO.initGear, 1, "OVER");
+  let g1 = SCENARIO.AddGear(16, s1);
+  let g2 = SCENARIO.AddFreeGear(16, { x: 0, y: 2, z: 12 });
+  SCENARIO.AddBelt(g1, g2, 0x103070);
 
   let s2 = SCENARIO.AddShaft(g2, 1, "OVER");
   let g3 = SCENARIO.AddGear(16, s2);
@@ -49,10 +71,6 @@ export function scenario5() {
   let g5 = SCENARIO.AddFreeGear(16, { x: -14, y: 0, z: 0 });
   SCENARIO.AddBelt(SCENARIO.initGear, g5);
 }
-
-export function scenario6() {}
-
-export function scenario7() {}
 
 export function scenario8() {
   const g1 = SCENARIO.AddGear(28, SCENARIO.initGear, "LEFT");
